6d834025447711621749178934efe91cf97c3591
Callum Dickinson <callum@huttradio.co.nz>
2015-09-08 15:42:51 +1200

    Configure koha-conf.xml with a config section

--- manifests/zebra/site.pp
+++ manifests/zebra/site.pp
@@ -238,6 +238,16 @@ define koha::zebra::site
 		}
 	}
 
+	unless (defined(::Koha::Files::Koha_conf_xml::Config[$site_name]))
+	{
+		::koha::files::koha_conf_xml::config
+		{ $site_name:
+			ensure			=> $ensure,
+			zebra_bib_index_mode	=> $indexing_mode,
+			zebra_auth_index_mode	=> $indexing_mode,
+		}
+	}
+
 	if ($biblioserver == true)
 	{
 		::koha::zebra::site::client
